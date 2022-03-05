19c7c178017dea1180a80ba170b84a6f83b7f151
Nobella-Nyarari-Ejiofor <nobella.ejiofor@student.moringaschool.com>
2022-03-05 09:20:58 -0500

    Logout route and view

--- park/urls.py
+++ park/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path , include, re_path
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('photos.urls')),
-    re_path(r'^accounts/' , include ('registration.backends.simple.urls'))
+    re_path(r'^accounts/' , include ('registration.backends.simple.urls')),
+    re_path(r'^logout/$' , views.LogoutView.as_view(), {"next_page":'/'}),
 ]
